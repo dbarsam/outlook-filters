7233737ec049f7ed8b01064d3414edff54581a32
dbarsam <dbarsam@gmail.com>
2013-02-04 14:35:26 -0500

    Added initial MoveAction action.

--- OutlookFilters/Actions/Action.cs
+++ OutlookFilters/Actions/Action.cs
@@ -3,6 +3,7 @@ using Microsoft.Office.Interop.Outlook;
 
 namespace OutlookFilters.Actions
 {
+    [XmlInclude(typeof(MoveAction))]
     public abstract class Action : IAction
     {
         public abstract bool Execute(MailItem item);
---
+++ OutlookFilters/Actions/MoveAction.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Xml.Serialization;
+using Microsoft.Office.Interop.Outlook;
+
+
+namespace OutlookFilters.Actions
+{
+    public class MoveAction : Action
+    {
+        #region Properties
+        public string DestinationPath 
+        {
+            get { return DestinationFolder != null ? DestinationFolder.FolderPath : String.Empty; }
+            set { DestinationFolder = FindFolder(value); }
+        }
+
+        [XmlIgnore]
+        public MAPIFolder DestinationFolder { get; set; }
+        #endregion
+
+        #region IAction Implementation
+        public override bool Execute(MailItem item)
+        {         
+            MAPIFolder folder = null;
+            item.Move(DestinationFolder);
+
+            return true;
+        }
+        #endregion
+
+        #region Protected Methods
+        protected MAPIFolder FindFolder(string path)
+        {
+            return null;            
+        }
+        #endregion
+    }
+}
